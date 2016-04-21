26304dd98603b554c747a4d0adaf440f2e8f23cf
darwinvaleny <darwin.o.valeny94@gmail.com>
2016-04-21 11:33:45 -0400

    Update MCtraj.m
    Cleaned up some comments and labeled plot. Variable 'im' in doPlot loop is having problems for some reason that I can't figure out. Matlab is saying it's undefined.

--- MCTraj/MCtraj.m
+++ MCTraj/MCtraj.m
@@ -9,13 +9,13 @@ addpath ../geom2d/geom2d
 
 C.q_0 = 1.60217653e-19;             % electron charge
 C.hb = 1.054571596e-34;             % Dirac constant
-C.h = C.hb * 2 * pi;                    % Planck constant
+C.h = C.hb * 2 * pi;                % Planck constant
 C.m_0 = 9.10938215e-31;             % electron mass
 C.kb = 1.3806504e-23;               % Boltzmann constant
 C.eps_0 = 8.854187817e-12;          % vacuum permittivity
 C.mu_0 = 1.2566370614e-6;           % vacuum permeability
 C.c = 299792458;                    % speed of light
-C.g = 9.80665 %metres (32.1740 ft) per s²
+C.g = 9.80665                       % metres (32.1740 ft) per sÂ²
 
 nTime = 100;
 nTraj = 10;
@@ -73,5 +73,7 @@ end
 if doPlot
     imwrite(im, map, 'imagefile.gif', 'DelayTime', 0, 'LoopCount', inf);
     figure
+    title('Trajectory')
+    xlabel('Distance')
 end
 
